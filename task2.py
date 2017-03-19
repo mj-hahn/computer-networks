47b51b63eef2835cdd8673a7cd7d5c70aceea24a
Noam <13nah@queensu.ca>
2017-03-19 00:55:26 -0400

    add verify function. doesn't work correctly due to wrong method of division

--- task2.py
+++ task2.py
@@ -1,6 +1,9 @@
+# define functions...
 def generator(message, polynomial):
-	# convert binary input to decimal
-	message_int = int(message, 2)
+	# find number of digits in polynomial
+	length = len(polynomial)
+	# convert binary input to decimal. also add length-1 0s
+	message_int = int(message+'0'*(length-1), 2)
 	polynomial_int = int(polynomial, 2)
 	# find the remainder
 	remainder = message_int % polynomial_int
@@ -9,9 +12,15 @@ def generator(message, polynomial):
 	codeword = str(message) + str(remainder).replace('b','')
 	return codeword;
 
-def verifier(message, polynomial):
-	print("verifier")
-	return
+def verifier(codeword, polynomial):
+	# convert binary input to decimal
+	codeword_int = int(codeword, 2)
+	polynomial_int = int(polynomial, 2)
+	# find the remainder
+	remainder = codeword_int % polynomial_int
+	# remainder should be 0 if message was valid
+	print (remainder)
+	return remainder == 0;
 
 def alter(codeword):
 	print("alter")
@@ -26,8 +35,14 @@ if set(message) <= valid:
 	polynomial = raw_input("Please enter a polynomial for CRC consisting of 1s and 0s\n")
 	# check input again
 	if set(polynomial) <= valid:
-		print(generator(message, polynomial))
+		# polynomial was valid, continue the program
+		# call the generator function
+		codeword = generator(message, polynomial)
+		print 'Codeword: ', codeword
+		print 'Polynomial: ', polynomial
+		# pass the codeword into the verifier
+		print(verifier(codeword, polynomial))
 	else:
-		print("invalid polynomial")
+		print("Invalid polynomial. Terminating program.")
 else:
-	print("invalid message")
+	print("Invalid message. Terminating program.")
