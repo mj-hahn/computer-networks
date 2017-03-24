da7e451421636eaf1fc37df66dcf38d45b8aadec
Noam <13nah@queensu.ca>
2017-03-24 15:36:36 -0400

    convert msg to binary padded

--- task1.py
+++ task1.py
@@ -1,7 +1,13 @@
 def application_layer(msg):
-	msg = "1" + msg
-	print msg
-	presentation_layer(msg)
+	msg_in_bits = ""
+	for c in msg:
+		# convert each letter to ascii
+		ascii_number = ord(c)
+		# convert each ascii to an 8-bit word (pad with zeros)
+		eight_bits = '{:08b}'.format(ascii_number)
+		msg_in_bits += eight_bits
+	print msg_in_bits
+	presentation_layer(msg_in_bits)
 	return
 def presentation_layer(msg):
 	msg = "1" + msg
